842571158a2f2716b6f26ed1067bc630c4066cb8
Jack Wearden <jack@jackwearden.co.uk>
2014-01-28 13:18:02 +0000

    Division & tests

--- src/testdemo/maths/Maths.java
+++ src/testdemo/maths/Maths.java
@@ -3,7 +3,10 @@ package testdemo.maths;
 public class Maths {
 
   public int add(int a, int b) {
-    return a + b;
+    return a * b;
   }
 
+  public float div(int a, int b) {
+    return a / b;
+  }
 }
